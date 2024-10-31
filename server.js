18e2a5b2a1539f8eb8e7302c6b7dc79a88fa8e04
danteesito <danteespinoza790@gmail.com>
2024-10-31 10:38:30 -0300

    cristian

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.set('view engine', 'ejs');``
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, 'scripts')));
+// Servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'scripts'))); // Servir scripts/app.js
 
 app.use('/', require('./routes/myRouter'));
 
-app.use((req, res, next) => {
-  res.status(404).render('404');
+// Middleware para el manejo de errores 404
+app.use((req, res) => {
+    res.status(404).render('404');
 });
 
 const port = 3000;
